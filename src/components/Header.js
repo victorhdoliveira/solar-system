b3b082abb0b83563bb49815bb4cacf2e5f3517b9
Victor Hugo <victorhdoliveira@gmail.com>
2022-09-20 20:20:02 -0300

    Requisito nº 1

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <header />
+        <h1 header>Sistema Solar</h1>
+      </>
+    );
+  }
+}
+
+export default Header;
