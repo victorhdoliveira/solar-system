5192c2f2400ea98401930cf5264b591825fc04a2
Victor Hugo <victorhdoliveira@gmail.com>
2022-09-20 20:25:53 -0300

    Requisito nº 2

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">Sistema Solar</div>
+    );
+  }
+}
+
+export default SolarSystem;
